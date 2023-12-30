4a1088a6d3e107e98474897d8f864680b1dd89e4
Prokop <prokop.schield@protonmail.com>
2023-12-31 00:14:55 +0100

    feat(src/run.ts): add options parameter

--- src/run.ts
+++ src/run.ts
@@ -1,14 +1,15 @@
-import { exec } from 'child_process';
+import { exec, ExecOptions } from 'child_process';
 
 /**
  * Executes shell command
  *
  * @param {string} cmd shell command
+ * @param {ExecOptions} options exec options
  * @returns {Promise<boolean>} Were there no errors?
  */
-function run(cmd: string): Promise<boolean> {
+function run(cmd: string, options: ExecOptions = {}): Promise<boolean> {
 	return new Promise((resolve) => {
-		const child = exec(cmd, (error, _stdout, stderr) =>
+		const child = exec(cmd, options, (error, _stdout, stderr) =>
 			resolve(!stderr && !error)
 		);
 		if (process.stdout && child.stdout)
